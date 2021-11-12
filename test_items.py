09e14be1ff15797c256f45c37f2db880ad65b2c8
AleksQD <AleksQD@gmail.com>
2021-11-12 19:44:48 +0300

    update conftest

--- test_items.py
+++ test_items.py
@@ -1,17 +1,16 @@
 import time
 
+
 def test_button_basket(browser):
-	link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-	
-	browser.implicitly_wait(5)
-	browser.get(link) 
-	# ищем все элементы с классом добавления в корзину	
-	button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
-	
-	# оставил тайм для проверки работы
-	# time.sleep(10)
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-	# проверяем что количество найденных элементов больше 0
-	assert button > 0, 'Отсутвует кнопка добавления в корзину'
+    browser.implicitly_wait(5)
+    browser.get(link)
+    # ищем все элементы с классом добавления в корзину
+    button = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
 
+    # оставил тайм для проверки работы
+    # time.sleep(10)
 
+    # проверяем что количество найденных элементов больше 0
+    assert button > 0, 'Отсутвует кнопка добавления в корзину'
